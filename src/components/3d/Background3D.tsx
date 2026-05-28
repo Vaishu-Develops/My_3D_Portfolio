901dc5c3321fe5c3695874c18a39f6f2f648acab
Vaishu-Develops <vaishnavisudarsanam11@gmail.com>
2026-05-28 21:16:14 +0530

    Add Background3D

---
+++ src/components/3d/Background3D.tsx
@@ -0,0 +1,34 @@
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Points, PointMaterial } from '@react-three/drei';
+// @ts-ignore
+import * as random from 'maath/random/dist/maath-random.esm';
+import * as THREE from 'three';
+
+export default function Background3D() {
+  const ref = useRef<THREE.Points>(null!);
+  // Create a sphere of random points
+  const sphere = random.inSphere(new Float32Array(5000 * 3), { radius: 10 });
+
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false}>
+        <PointMaterial
+          transparent
+          color="#aa3bff"
+          size={0.05}
+          sizeAttenuation={true}
+          depthWrite={false}
+          blending={THREE.AdditiveBlending}
+        />
+      </Points>
+    </group>
+  );
+}
